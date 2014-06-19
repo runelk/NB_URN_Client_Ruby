e014e76806794a1e0dd1adb06f0b4d3814345c4c
Rune Lain Knudsen <rune.knudsen@uib.no>
2014-06-19 15:20:09 +0200

    Done converting library to Ruby (mostly untested).

--- lib/nb_urn_client.rb
+++ lib/nb_urn_client.rb
@@ -10,34 +10,41 @@ class NbUrnClient
 
   def initialize(opts={})
     defaults = {config_file: nil, username: nil, password: nil}
+    opts = opts.inject({}){|h,(k,v)| h[k.to_sym] = v; h}
     opts = defaults.merge(opts)
     @config = YAML.load(open(opts[:config_file]))['config']['urn']
-    @client = SOAP::WSDLDriverFactory.new(@config['wsdl']).create_rpc_driver
-    @client.return_response_as_xml = true
+    @config = @config.inject({}){|h,(k,v)| h[k.to_sym] = v; h}
+    @client = SOAP::WSDLDriverFactory.new(@config[:wsdl]).create_rpc_driver
     @sso_token = nil
-    @config['username'] = username ||= @config['username']
-    @config['password'] = password ||= @config['password']
+    @config[:username] = opts[:username] ||= @config[:username]
+    @config[:password] = opts[:password] ||= @config[:password]
   end
 
   def add_url(urn, url)
-    if @sso_token
-      return @client.addURL(@sso_token, urn, url)
+    if not @sso_token.nil?
+      return @client.addURL({'SSOToken' => @sso_token,
+                              'URN' => urn,
+                              'URL' => url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
   
   def create_urn(series_code, url)
-    if @sso_token
-      return @client.createURN(@sso_token, series_code, url)
+    if not @sso_token.nil?
+      return @client.createURN({'SSOToken' => @sso_token, 
+                                 'seriesCode' => series_code,
+                                 'URL' => url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def delete_url(urn, url)
-    if @sso_token
-      return @client.deleteURL(@sso_token, urn, url)
+    if not @sso_token.nil?
+      return @client.deleteURL({'SSOToken' => @sso_token, 
+                                 'URN' => urn,
+                                 'URL' => url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
@@ -52,63 +59,72 @@ class NbUrnClient
   end
 
   def get_next_urn(series_code)
-    if @sso_token
-      return @client.getNextURN(@sso_token, series_code)
+    if not @sso_token.nil?
+      return @client.getNextURN({'SSOToken' => @sso_token, 
+                                  'seriesCode' => series_code})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def login()
-    return @client.login(@client.config['username'], @client.config['password'])
+    result = @client.login({'username' => @config[:username],
+                             'password' => @config[:password]})
+    @sso_token = result['SSOToken']
+    return @sso_token
   end
 
-  # def login(username, password)
-  #   return @client.login(username, password)
-  # end
-
   def logout()
-    if @sso_token
-      @client.service.logout(@sso_token)
+    if not @sso_token.nil?
+      @client.logout({'SSOToken' => @sso_token})
       @sso_token = nil
     end
   end
 
   def register_urn(urn, url)
-    if @sso_token
-      return @client.registerURN(@sso_token, urn, url)
+    if not @sso_token.nil?
+      return @client.registerURN({'SSOToken' => @sso_token,
+                                   'URN' => urn,
+                                   'URL' => url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def replace_url(urn, old_url, new_url)
-    if @sso_token
-      return @client.replareURL(sso_token, urn, old_urn, new_url)
+    if not @sso_token.nil?
+      return @client.replareURL({'SSOToken' => @sso_token, 
+                                  'URN' => urn, 
+                                  'oldURL' => old_url,
+                                  'newURL' => new_url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def reserve_next_urn(series_code)
-    if @sso_token
-      return @client.reserveNextURN(@sso_token, series_code)
+    if not @sso_token.nil?
+      return @client.reserveNextURN({'SSOToken' => @sso_token, 
+                                      'seriesCode' => series_code})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def reserve_urn(urn)
-    if @sso_token
-      return @client.reserveURN(@sso_token, urn)
+    if not @sso_token.nil?
+      return @client.reserveURN({'SSOToken' => @sso_token, 
+                                  'URN' => urn})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
   end
 
   def set_default_url(urn, url)
-    if @sso_token
-      return @client.setDefaultURL(@sso_token, urn, url)
+    if not @sso_token.nil?
+      return @client.setDefaultURL({'SSOToken' => @sso_token, 
+                                     'URN' => urn,
+                                     'URL' => url})
     else
       raise SsoTokenError("No SSO token available. You need to login first.")
     end
